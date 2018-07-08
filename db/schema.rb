b3113306c14c5e1da29157785ee49676a75cb020
yungcheng <yungcheng111023@gmail.com>
2018-07-08 21:00:39 +0800

    add css stting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708085608) do
+ActiveRecord::Schema.define(version: 20180708123953) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180708085608) do
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
   end
 
 end
