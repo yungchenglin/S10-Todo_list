3925cac3b45b92fdee5b50620a2f5c5e6a7681da
yungcheng <yungcheng111023@gmail.com>
2018-07-08 20:21:25 +0800

    set DRY and Bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
  class TasksController < ApplicationController
-
+    before_action :set_task, :only => [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -10,34 +10,31 @@
 
   def create
     @task = Task.new(task_params)
-    @task.save
-
-    redirect_to tasks_url
-  end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def edit
-    @task = Task.find(params[:id])
+    if  @task.save
+        redirect_to tasks_url
+    else
+      render :action => :new
+    end
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update_attributes(task_params)
-
-    redirect_to task_path(@task)
+    if @task.update_attributes(task_params)
+      redirect_to task_path(@task)
+    else
+      render :action => :edit
+    end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
     redirect_to tasks_url
 end
  
 private
+
+def set_task
+  @task = Task.find(params[:id])
+end
 
 def task_params
   params.require(:task).permit(:name, :due_date, :note)
